dfcf54057e9dc5c909a83dd3fbbd5b33c36c3056
shinkeonkim <singun11@naver.com>
2020-01-16 18:12:18 +0900

    index.html template 밑바탕 제작

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.listen(3030,function(){
     console.log("server on port 3030");
 })
 
+app.use(express.static('public'))
+app.use(express.static('style'))
+
 app.get('/',function(request,response){
     response.sendFile(__dirname + "/public/index.html")
 })
