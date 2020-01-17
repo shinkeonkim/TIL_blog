32a0969b8037c87136ff5b992b919e05fd485cf3
shinkeonkim <singun11@naver.com>
2020-01-17 14:25:52 +0900

    posts function md to html transform, add css

--- app.js
+++ app.js
@@ -1,14 +1,89 @@
-var express = require('express')
+const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+const fs = require('fs')
+const hljs = require('highlight.js')
+const md = require('markdown-it') ({
+    html: false,
+    xhtmlOut: false,
+    breaks: false,
+    langPrefix: "lanuage-",
+    linkify: true,
+    typographer: true,
+    highlight: function(str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return (
+              '<pre class="hljs"><code>' +
+              hljs.highlight(lang, str, true).value +
+              "</code></pre>"
+            );
+          } catch (__) {}
+        }
+    
+        return (
+          '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + "</code></pre>"
+        );
+      }
+});
 
 var app =express()
+
+var postDirectoryPath = path.join(__dirname + "/posts")
+var postFiles = fs.readdirSync(postDirectoryPath)
+
+getHtmlFileName = file => {
+    return file.slice(0,file.indexOf(".")).toLowerCase();
+}
+
+const deployDir = '/deploy';
+if (!fs.existsSync(deployDir)) {
+    fs.mkdirSync(deployDir);
+}
+const deployFiles = [];
+
+
+const articleHtmlFormat = fs.readFileSync(
+    "./views/article_format.ejs",
+    "utf8"
+)
+
+const postHtmlFormat = fs.readFileSync(
+    "./views/post_format.ejs",
+    "utf8"
+)
+
 
 app.listen(3030,function(){
     console.log("server on port 3030");
 })
 
+app.use(express.static('deploy'))
 app.use(express.static('public'))
 app.use(express.static('style'))
+app.use(express.static('node_modules'))
 
 app.get('/',function(request,response){
     response.sendFile(__dirname + "/public/index.html")
+    
 })
+
+
+
+
+
+postFiles.map(file => {
+    const body = fs.readFileSync(`./posts/${file}`,"utf8");
+    const convertBody = md.render(body);
+    const articleHtml = ejs.render(articleHtmlFormat, {
+        body: convertBody
+    })
+    
+    const postHtml = ejs.render(postHtmlFormat,{
+        title: "happykoa",
+        content: articleHtml
+    });
+    const fileName = getHtmlFileName(file);
+    fs.writeFileSync(`./deploy/${fileName}.html`,postHtml);
+    deployFiles.push(fileName); 
+});
---
+++ posts/test1.md
@@ -0,0 +1,15 @@
+```cpp
+#include <iostream>
+using namespcae std;
+int main() {
+
+}
+```
+
+test 텍스트
+
+```python
+a = input()
+b = input()
+print(int(a)+int(b))
+```
